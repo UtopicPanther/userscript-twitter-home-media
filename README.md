efafc69418404bc518752b16080c5efbf7cafa8b
UtopicPanther <65122621+UtopicPanther@users.noreply.github.com>
2020-08-06 01:20:06 +0800

    first beta version 0.5.1

--- README.md
+++ README.md
@@ -1,25 +1,58 @@
-## Media mode for Twitter home (ALPHA!!!)
+## Media mode for Twitter home (Beta)
 
-Remove text-only tweets on the flow of my Twitter home.
+Remove text-only tweets on the flow of my Twitter home and list.
 
-Script need some time to startup. After started, a dialog will be shown.
+Script need some time to startup. 
 
-MUST be loaded after twitter loaded the flow, or not working.
+**Warning: There is no completely accurate way to detect whether a tweet contains pictures and other media. This script has a lot of redundant processing, and accidental deletion of tweets should be quite rare. But there may still be accidental deletion. If it is not acceptable, please do not use this script.**
 
-Install URL: https://raw.githubusercontent.com/UtopicPanther/userscript-twitter-home-media/master/twitter-home-media.user.js
+How it works: Monitor page element changes. Then retrieve all tweets that do not contain pictures and other media (such as videos) and hide them.
 
-## Twitter 主页面上的媒体模式 （ALPHA！）
+I only tested in Firefox (disabled CSP) and Violentmonkey. User reports also work under Chrome.
 
-在我的 Twitter 主页面上删除纯文本的 Tweet
+## Twitter 主页面上的媒体模式 （Beta）
 
-脚本需要一定的时间才能启动。启动后，将弹出一个对画框
+在我的 Twitter 主页和列表上删除纯文本的 Tweet。
 
-**必须** 在 Twitter 加载出时间线后再启动，否则不工作
+脚本需要一定的时间才能启动。
 
-安装 URL：https://raw.githubusercontent.com/UtopicPanther/userscript-twitter-home-media/master/twitter-home-media.user.js
+**警告：不存在完全准确的方式探测 Tweet 是否包含图片和其他媒体。此脚本进行了大量冗余处理，出现误删除 Tweet 的情况应该是相当罕见的。但仍然可能出现误删除的情况，如果不能接受，请勿使用此脚本。**
 
-### Firefox tampermonkey notice
+工作原理：监听页面元素变动。然后检索不包含图片和其他媒体（如视频）的全部 Tweet，将它们隐藏。
+
+我仅在 Firefox （禁用 CSP）和 Violentmonkey 环境下测试。用户报告也在 Chrome 下工作。
+
+### Firefox notice
 
 Due to CSP, maybe you need set `security.csp.enable` in your Firefox `about:config` to `false`
 
-For more infomation, see: https://github.com/Tampermonkey/tampermonkey/issues/952
+由于 CSP 限制，你可能需要在新版 Firefox 高级首选项 `about:config` 中将 `security.csp.enable` 设置为 `false`
+
+### Install
+
+Github: https://raw.githubusercontent.com/UtopicPanther/userscript-twitter-home-media/master/twitter-home-media.user.js 
+
+### COPYING
+
+Copyright (C) 2020 UtopicPanther
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+This script contains an additional exemption. When this script is
+injected into the site with the original setting of `@match`, 
+other user scripts running in the same space are not required to
+be compatible with GPL 3.
+
+这个脚本包含一个附加豁免。当此脚本被注入到原有设置 `@match` 的
+站点时，不要求同一空间中运行的其他用户脚本与 GPL 3 兼容。
--- twitter-home-media.user.js
+++ twitter-home-media.user.js
@@ -1,22 +1,52 @@
 // vim: set ts=4 sw=4 expandtab:
 
 // ==UserScript==
-// @name         Media mode for Twitter home
-// @namespace    https://github.com/UtopicPanther/userscript-twitter-home-media
-// @supportURL   https://github.com/UtopicPanther/userscript-twitter-home-media/issues
-// @version      0.4.1
-// @description  Remove text-only tweet on the flow of Twitter home/list
-// @author       UtopicPanther
-// @match        https://twitter.com/*
-// @match        https://mobile.twitter.com/*
-// @grant        none
-// @run-at       document-idle
+// @name               Media mode for Twitter home
+// @name:zh-CN         Twitter 主页上的媒体模式
+// @name:zh-TW         Twitter 主頁上的媒體模式
+// @name:zh-HK         Twitter 主頁上的媒體模式
+// @description        Remove text-only tweets on the flow of Twitter home/list. It is currently Beta quality.
+// @description:zh-CN  在 Twitter 的主页和列表时间流上删除纯文本 Tweet。当前是 Beta 质量
+// @description:zh-TW  在 Twitter 的主頁和列表時間流上刪除純文字 Tweet。當前是 Beta 質量
+// @description:zh-HK  在 Twitter 的主頁和列表時間流上刪除純文本 Tweet。當前是 Beta 質量
+// @icon               https://i.imgur.com/bUIPv1O.jpg
+// @namespace          https://github.com/UtopicPanther/userscript-twitter-home-media
+// @supportURL         https://github.com/UtopicPanther/userscript-twitter-home-media/issues
+// @version            0.5.1
+// @author             UtopicPanther
+// @match              https://twitter.com/*
+// @match              https://mobile.twitter.com/*
+// @grant              none
+// @run-at             document-idle
 // ==/UserScript==
+
+/*
+ *  Copyright (C) 2020 UtopicPanther
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  This script contains an additional exemption. When this script is
+ *  injected into the site with the original setting of `@match`, 
+ *  other user scripts running in the same space are not required to
+ *  be compatible with GPL 3.
+ *
+ *  这个脚本包含一个附加豁免。当此脚本被注入到原有设置 `@match` 的
+ *  站点时，不要求同一空间中运行的其他用户脚本与 GPL 3 兼容。
+ */
 
 (function() {
     'use strict';
-
-    //let intersectionObserver;
 
     const removeTweet = article => {
         article.classList.add('mmfth_hide');
@@ -49,7 +79,16 @@
         if (!haveVideo)
             haveVideo = (i.querySelector('div[role=progressbar]') != null);
 
-        return (!haveImages && !haveVideo);
+        let haveNonEmptyMiddle = false;
+
+        try {
+            const tweet = i.querySelector('div[data-testid=tweet]');
+            const middle = tweet.children[1].children[1].children[1];
+            if (middle.children.length > 0)
+                haveNonEmptyMiddle = true;
+        } catch (e) {}
+
+        return (!haveImages && !haveVideo && !haveNonEmptyMiddle);
     }
 
     const findTweetsForRemove = () => {
@@ -57,8 +96,6 @@
             location.pathname.startsWith('/i/list/')) {
             document.querySelectorAll('article:not(.mmfth_hide)').forEach(i => {
                 if (isTweetOnlyText(i)) {
-                    //console.log("add observ %O", i);
-                    //intersectionObserver.observe(i);
                     removeTweet(i);
                 }
             });
@@ -79,21 +116,6 @@
     }
 
     setTimeout(() => {
-        alert("Media mode for Twitter home will be actived. After your flow loaded, click Yes.");
         startObserver();
-/*
-        const options = {
-            root: null,
-            rootMargin: '0px',
-            threshold: 1.0
-        }
-        intersectionObserver = new IntersectionObserver((es, o) => {
-            es.forEach(i => {
-                if (isTweetOnlyText(i.target))
-                    removeTweet(i.target)
-                o.unobserve(i.target);
-            });
-        }, options);
-*/
-    }, 4000);
+    }, 6000);
 })();
